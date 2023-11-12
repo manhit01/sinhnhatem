62e2cda59bca1fd45c99f0d2a8a5ac281946b5ae
manhit01 <128140274+manhit01@users.noreply.github.com>
2023-11-12 10:49:53 +0700

    Add files via upload
    Chúc mừng sinh nhật em

---
+++ config.js
@@ -0,0 +1,13 @@
+const nameGirl = 'Em';
+const giftUrl = '';
+const eventName = 'Chúc Mừng Sinh Nhật Em ';
+const titleCard = 'Tặng Người Anh IU ';
+const contentCard = 'Chúc honey của anh sinh nhật tràn ngập niềm vui và những nụ cười. Mong điều đẹp nhất sẽ đến với em trong hôm nay và cả những ngày sau';
+
+// phần dưới dành cho các bạn biết code, nếu muốn chỉnh ảnh đơn giản với base64
+// Cần hỗ trợ hãy liên hệ: 
+// Mr-Nam http://facebook.com/nam.nodemy
+// Các bạn muốn học lập trình thì tham gia Nhóm zalo tự học lập trình nhé: https://zalo.me/g/yhdkef092
+const giftImage = 'hot-girl.png';
+const base64 = '';s
+const giftImageBase64 = "data:image/png;base64, " + base64;
